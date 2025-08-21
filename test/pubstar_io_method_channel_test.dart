b1986dd3766b2e6ba6239e3deb06523972f01723
Elon Nguyen <elon.nguyen@tqcsolution.com>
2025-08-21 11:18:29 +0700

    remote import on unit test

--- test/pubstar_io_method_channel_test.dart
+++ test/pubstar_io_method_channel_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:pubstar_io/pubstar_io.dart';
-import 'package:pubstar_io/src/callback_handler.dart';
 import 'package:pubstar_io/src/method_channel_name.dart';
 import 'package:pubstar_io/src/pubstar_io_method_channel.dart';
 import 'package:pubstar_io/src/pubstar_io_exception.dart';
