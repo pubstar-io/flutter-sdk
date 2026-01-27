fc3d844582f775c9d27c0d842658eac225ba8449
Elon Nguyen <elon.nguyen@tqcsolution.com>
2026-01-27 13:55:20 +0700

    Release 1.5.0

--- example/android/build.gradle.kts
+++ example/android/build.gradle.kts
@@ -2,10 +2,6 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-        
-        maven {
-            url = uri("https://artifactory.appodeal.com/appodeal")
-        }
     }
 }
 
